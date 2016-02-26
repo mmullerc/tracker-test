fb87c909e60ab51939568f3489b23a258e787753
fabriciocb10 <fabriciocb10@hotmail.com>
2016-02-26 10:22:47 -0600

    traking contact elements

--- client/app/contact/contact.controller.js
+++ client/app/contact/contact.controller.js
@@ -1,32 +1,24 @@
 'use strict'
 ;(function(){
 
-	// class ContactCtrl{
-
-	// 	constructor(){}
-	// }
-
-	// angular.module('trackerTestApp')
-	// 	.controller('ContactCtrl', ContactCtrl);
 	angular.module('trackerTestApp')
    .controller('ContactCtrl', ContactCtrl);
 
-   function ContactCtrl() {
-     var vm = this;
-     vm.DoSomething = 'Don\'t scream';
-     console.log(vm.DoSomething);
-   };
+   function ContactCtrl(clickService) {
+   	//console.log(' dub');
+     var vmContact = this;
+     vmContact.title = {'id':'1','url':'/contact', 'element':'Contact title', 'src':'CONTACT'};
+     vmContact.img = {'id':'2','url':'/contact', 'element':'Body Image', 'src':'../assets/images/flowers.png'};
+     vmContact.facebook = {'id':'3','url':'/contact', 'element':'Facebook Link'};
+     vmContact.twitter = {'id':'4','url':'/contact', 'element':'Twitter Link'};
+     vmContact.linkedin = {'id':'5','url':'/contact', 'element':'Linkedin Link'};
+     vmContact.sendButton = {'id':'6','url':'/contact', 'element':'Form'};
+     
 
+     vmContact.register = function(psection){
+     clickService.addClick(psection);
+   };
+ }
 
 })();
 
-// ;(function () {
-//   app.module('trackerTestApp')
-//     .controller('ContactCtrl', ContactCtrl);
-
-//     function ContactCtrl() {
-//       var vm = this;
-//       vm.DoSomething = 'Don\'t scream';
-//       console.log(vm.DoSomething);
-//     }
-// })();	
