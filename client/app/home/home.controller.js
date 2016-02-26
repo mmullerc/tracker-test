1f4935ee2be28eb167d56f84cb4c2225189e09d1
Pedro Gutierresz <pedrogutierresz@MacBook-Pro-de-Pedro.local>
2016-02-26 15:45:23 -0600

    Ids mistake

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -6,11 +6,11 @@ angular.module('trackerTestApp')
 
  function HomeController(clickService) {
    var vmHome = this;
-   vmHome.section1 = {'id':'1','url':'/home', 'element': 'Welcome title','src':'WELCOME'};//title
-   vmHome.section2 = {'id':'2','url':'/home', 'element': 'Paragraph 1','src':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'};//subtitle
-   vmHome.section3 = {'id':'3','url':'/home', 'element': 'Body image', 'src':'../assets/images/home.png'};//image
-   vmHome.section4 = {'id':'4','url':'/home', 'element': 'Your Test title','src':'YOUR TEST'};
-   vmHome.section5 = {'id':'2','url':'/home', 'element': 'Paragraph 2','src':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'};//subtitle
+   vmHome.section1 = {'id':'home-1','url':'/home', 'element': 'Welcome title','src':'WELCOME'};//title
+   vmHome.section2 = {'id':'home-2','url':'/home', 'element': 'Paragraph 1','src':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'};//subtitle
+   vmHome.section3 = {'id':'home-3','url':'/home', 'element': 'Body image', 'src':'../assets/images/home.png'};//image
+   vmHome.section4 = {'id':'home-4','url':'/home', 'element': 'Your Test title','src':'YOUR TEST'};
+   vmHome.section5 = {'id':'home-2','url':'/home', 'element': 'Paragraph 2','src':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'};//subtitle
    vmHome.register = function(p) {
      clickService.addClick(p);
 
