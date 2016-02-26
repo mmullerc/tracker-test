6597d8f8a4d8777aa62fdd932738cf52468be848
fabriciocb10 <fabriciocb10@hotmail.com>
2016-02-26 14:37:40 -0600

    id contact changes

--- client/app/contact/contact.controller.js
+++ client/app/contact/contact.controller.js
@@ -7,12 +7,12 @@
    function ContactCtrl(clickService) {
    	//console.log(' dub');
      var vmContact = this;
-     vmContact.title = {'id':'1','url':'/contact', 'element':'Contact title', 'src':'CONTACT'};
-     vmContact.img = {'id':'2','url':'/contact', 'element':'Body Image', 'src':'../assets/images/flowers.png'};
-     vmContact.facebook = {'id':'3','url':'/contact', 'element':'Facebook Link'};
-     vmContact.twitter = {'id':'4','url':'/contact', 'element':'Twitter Link'};
-     vmContact.linkedin = {'id':'5','url':'/contact', 'element':'Linkedin Link'};
-     vmContact.sendButton = {'id':'6','url':'/contact', 'element':'Form'};
+     vmContact.title = {'id':'contact1','url':'/contact', 'element':'Contact title', 'src':'CONTACT'};
+     vmContact.img = {'id':'contact2','url':'/contact', 'element':'Body Image', 'src':'../assets/images/flowers.png'};
+     vmContact.facebook = {'id':'contact3','url':'/contact', 'element':'Facebook Link'};
+     vmContact.twitter = {'id':'contact4','url':'/contact', 'element':'Twitter Link'};
+     vmContact.linkedin = {'id':'contact5','url':'/contact', 'element':'Linkedin Link'};
+     vmContact.sendButton = {'id':'contact6','url':'/contact', 'element':'Form'};
      
 
      vmContact.register = function(psection){
